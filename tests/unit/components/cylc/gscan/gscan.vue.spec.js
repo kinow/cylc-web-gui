befd8f828819dd4d6b3623fa31aad236d0ba2465
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2021-07-22 11:50:10 +1200

    Fix sorting by type, include unit test

--- tests/unit/components/cylc/gscan/gscan.vue.spec.js
+++ tests/unit/components/cylc/gscan/gscan.vue.spec.js
@@ -220,6 +220,13 @@ describe('GScan component', () => {
             { name: 'a/b', status: WorkflowState.RUNNING }
           ]),
           expected: ['b', 'a']
+        },
+        {
+          workflows: createWorkflows([
+            { name: 'a/b', status: WorkflowState.RUNNING },
+            { name: 'a', status: WorkflowState.RUNNING }
+          ]),
+          expected: ['b', 'a']
         }
       ]
       tests.forEach(test => {
